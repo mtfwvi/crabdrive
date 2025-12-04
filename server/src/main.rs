eb3910b1c153ee8561a1366a2f0c75a938aeb7ad
mtfwvi <72407233+mtfwvi@users.noreply.github.com>
2025-12-04 14:17:00 +0100

    server: Init server crate (crabdrive-server) (#19)
    * server: Init server crate (crabdrive-server)
    * server: Make server address configurable via env
    * server/docs: Document CRABDRIVE_ADDR env variable

---
+++ server/src/main.rs
@@ -0,0 +1,25 @@
+use axum::{
+    routing::get,
+    Router
+};
+use tokio::signal;
+
+async fn cc_signal() {
+    let _ = signal::ctrl_c().await;
+    println!("Exiting");
+}
+
+#[tokio::main]
+async fn main() {
+    let addr = std::env::var("CRABDRIVE_ADDR").unwrap_or("127.0.0.1:2722".to_string());
+
+    let app = Router::new().route("/", get(|| async { "Hello Crabdrive!" }));
+    if let Ok(listener) =  tokio::net::TcpListener::bind(&addr).await {
+        println!("Server running on http://{}", &addr);
+        axum::serve(listener, app)
+            .with_graceful_shutdown(cc_signal())
+            .await.unwrap();
+    } else {
+        panic!("Cannot bind TcpListener. Is the port in use?")
+    }
+}
