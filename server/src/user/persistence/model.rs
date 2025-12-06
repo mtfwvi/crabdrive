544d0053864afabc54d1f82f88c74fc2a5508798
dercodeling <ichmagauchschnitzel@gmail.com>
2025-12-06 21:43:11 +0100

    feat(server/user): Start defining User and UserRepository

--- server/src/user/persistence/model.rs
+++ server/src/user/persistence/model.rs
@@ -1,1 +1,13 @@
-pub(crate) struct User {}
+pub struct DataAmount {
+    // TODO: Move to commons/model
+    pub byte_count: u64,
+}
+
+pub(crate) struct User {
+    id: i32,
+    email: String,
+    username: String,      // TODO: Both username and email? Discuss uses
+    password_hash: String, // TODO: Correct? Anything else needed for auth?
+    storage_limit: DataAmount,
+    // TODO: What fields for encryption?
+}
--- server/src/user/persistence/repository.rs
+++ server/src/user/persistence/repository.rs
@@ -1,1 +1,4 @@
-pub(crate) trait UserRepository {}
+pub(crate) trait UserRepository {
+    fn create_user(username: String, email: String, password_hash: String);
+    fn delete_user(user_id: i32);
+}
