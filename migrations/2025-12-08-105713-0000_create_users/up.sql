1417fdb82a1389a18a448bff09e3d038da6f75a4
Jonas Bretz <jbretz43@gmail.com>
2025-12-08 15:57:33 +0100

    Updated user table

--- migrations/2025-12-08-105713-0000_create_users/up.sql
+++ migrations/2025-12-08-105713-0000_create_users/up.sql
@@ -4,8 +4,23 @@ CREATE TABLE users (
     email TEXT NOT NULL UNIQUE,
     password_hash TEXT NOT NULL,
     created_at TEXT NOT NULL,
-    updated_at TEXT NOT NULL
+    updated_at TEXT NOT NULL,
+    storage_limit INTEGER,
+    masterkey BINARY NOT NULL REFERENCES encryptionKey(ekey)
+    private_key BINARY NOT NULL REFERENCES encryptionKey(ekey)
+    public_key BINARY NOT NULL
+    root_key BINARY NOT NULL REFERENCES encryptionKey(ekey)
+    root_node INTEGER NULL
+    trash_key BINARY NOT NULL REFERENCES encryptionKey(ekey)
+    trash_node BINARY NOT NULL REFERENCES encryptionKey(ekey)
+    
 );
+
+
+CREATE TABLE encryptionKey(
+    ekey BINARY NOT NULL,
+    IV TEXT NOT NULL
+)
 
 CREATE INDEX idx_users_email ON users(email);
 CREATE INDEX idx_users_username ON users(username);
