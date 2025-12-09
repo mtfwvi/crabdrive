956c23b6e31cbc80fc6add54919e7a23e8bb7489
dercodeling <ichmagauchschnitzel@gmail.com>
2025-12-09 10:43:25 +0100

    refactor: Move database files to server

--- server/src/db/mod.rs
+++ server/src/db/mod.rs
@@ -4,4 +4,4 @@ pub mod schema;
 pub mod connection;
 pub mod operations;
 
-pub use connection::{establish_connection, create_pool, DbPool};
+pub use connection::{create_pool, establish_connection, DbPool};
