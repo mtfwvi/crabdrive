9e23e54b4c41dc999493bbe1c3fb376ae5881328
trmdnt <180674832+trmdnt@users.noreply.github.com>
2025-12-14 23:59:51 +0100

    feat(server/entities): added metadata change counter to avoid track changes and avoid overwriting them during conflicts

--- server/src/storage/node/persistence/model/node_entity.rs
+++ server/src/storage/node/persistence/model/node_entity.rs
@@ -20,9 +20,16 @@ pub struct NodeEntity {
     /// The time the file was moved to the trash as unix time (None for a not deleted node)
     deleted_on: Option<u64>,
 
+    /// Counter that indicates the amount of times the metadata was updated. The server should
+    /// increment it during each metadata update. Before accepting a change from a client, the
+    /// server should check that the counter indicated by the client matches and return an error if
+    /// it doesn't. This avoids loosing data when two clients try to change the metadata
+    /// simultaneously
+    metadata_change_counter: u64,
+
     /// The version of the file that is currently active (None for none-file nodes).
     /// May point to an incomplete version when the file was just created and is being uploaded
-    current_version: Option<VersionId>,
+    current_file_version: Option<VersionId>,
 
     //TODO find out how to store this in the database (string or int)
     node_type: NodeType,
