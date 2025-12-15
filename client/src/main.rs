6298a2ae4b537ccaaddff85e70dbce3072dcafa0
dercodeling <ichmagauchschnitzel@gmail.com>
2025-12-15 17:29:21 +0100

    feat(client): Move out component to separate module

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,12 +1,10 @@
+mod pages;
+
 use leptos::prelude::*;
+use pages::demo_page::DemoPage;
 
 fn main() {
     console_error_panic_hook::set_once();
 
-    mount_to_body(|| {
-        view! {
-            <h1>crabdrive</h1>
-            <p>Rust native cloud storage</p>
-        }
-    })
+    mount_to_body(DemoPage)
 }
---
+++ client/src/pages/demo_page.rs
@@ -0,0 +1,9 @@
+use leptos::prelude::*;
+
+#[component]
+pub(crate) fn DemoPage() -> impl IntoView {
+    view! {
+        <h1>"crabdrive 🦀"</h1>
+        <h3>"Rust native cloud storage ⚙️"</h3>
+    }
+}
