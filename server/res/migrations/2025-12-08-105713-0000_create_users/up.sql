668291b6c5d2761295dec9730999710d2a8d37b8
Jonas Bretz <jbretz43@gmail.com>
2025-12-18 22:40:52 +0100

    Database adaptation for final crates

--- server/res/migrations/2025-12-08-105713-0000_create_users/up.sql
+++ server/res/migrations/2025-12-08-105713-0000_create_users/up.sql
@@ -4,8 +4,7 @@ CREATE TABLE users (
     username TEXT NOT NULL UNIQUE,
     password_hash TEXT NOT NULL,
     storage_limit INTEGER,
-    user_type TEXT,
-    created_at TIMESTAMP,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     encryption_uninitialized INTEGER DEFAULT 0,
     masterkey BLOB NOT NULL,
     masterkey_iv BLOB NOT NULL,
@@ -14,32 +13,35 @@ CREATE TABLE users (
     public_key BLOB NOT NULL,
     rootkey BLOB NOT NULL,
     rootkey_iv BLOB NOT NULL,
-    root_node INTEGER NULL,
+    root_node TEXT NULL,
     trashkey BLOB NOT NULL,
     trashkey_iv BLOB NOT NULL,
-    trash_node BLOB NOT NULL 
+    trash_node TEXT NOT NULL 
 );
 
 CREATE INDEX idx_users_username ON users(username);
-
-CREATE TABLE revision(
-    id TEXT,
-    FOREIGN KEY file_id REFERENCES Node(id),
-    upload_started_on TIMESTAMP NOT NULL,
-    upload_ended_on TIMESTAMP
-    iv BLOB
-);
 
 CREATE TABLE Node(
     id TEXT PRIMARY KEY NOT NULL,
     parent_id TEXT DEFAULT NULL, 
     owner_id TEXT NOT NULL,
     mdata BLOB NOT NULL,
-    iv TEXT NOT NULL REFERENCES encryptionKey(IV)
+    iv BLOB NOT NULL,
     deleted_on TIMESTAMP,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     metadata_change_counter INTEGER NOT NULL DEFAULT 0,
-    FOREIGN KEY current_revision REFERENCES revision(id),
-    node_type TEXT
+    current_revision TEXT,
+    node_type TEXT,
+    FOREIGN KEY (owner_id) REFERENCES users(id),
+    FOREIGN KEY (parent_id) REFERENCES Node(id),
+    FOREIGN KEY (current_revision) REFERENCES revision(id)
 );
 
--- find out how to show creation time on default when creating a user, file, etc
+CREATE TABLE revision(
+    id TEXT PRIMARY KEY NOT NULL,
+    fid TEXT NOT NULL,
+    upload_started_on TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    upload_ended_on TIMESTAMP,
+    iv BLOB,
+    FOREIGN KEY (fid) REFERENCES Node(id)
+);
