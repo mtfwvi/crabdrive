e1e96691e18f19dd47e834731282c957848db9a4
Jonas Bretz <jbretz43@gmail.com>
2025-12-20 10:27:54 +0100

    Refactor code for Rustfmt

--- server/src/db/connection.rs
+++ server/src/db/connection.rs
@@ -5,7 +5,6 @@ use std::env;
 
 pub type DbPool = Pool<ConnectionManager<SqliteConnection>>;
 
-
 pub fn establish_connection() -> SqliteConnection {
     dotenv().ok();
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
--- server/src/db/operations.rs
+++ server/src/db/operations.rs
@@ -4,4 +4,3 @@
 //replaceFile (nodeId, node_metadata) -> Ok/Error // copies file node in temporary db withUploadStarted timestamp
 //uploadChunk (nodeId) -> Ok/Error
 //finishUpload (nodeId) -> Ok/Error // replaces the old node with the node in the temporary db (this makes it impossible to end in an invalid state)
-
