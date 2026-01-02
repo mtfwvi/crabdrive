8966651ec2f6655e5ce00bc200c42cfd2cf2dc2e
mtfw <72407233+mtfwvi@users.noreply.github.com>
2026-01-02 13:26:21 +0100

    refactor(server/http): Rename lat to response_time

--- server/src/http/middleware.rs
+++ server/src/http/middleware.rs
@@ -1,9 +1,7 @@
 use axum::{http::Request, middleware::Next, response::Response};
 use std::time::Instant;
 
-use crate::http::AppState;
 use axum::body::Body;
-use axum::extract::State;
 use axum::http::{HeaderName, HeaderValue};
 use tracing::{debug, trace_span};
 
