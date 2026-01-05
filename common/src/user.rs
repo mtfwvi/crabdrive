761fce297aa2c93858fa56262d1837186cf33a49
Jonas Bretz <jbretz43@gmail.com>
2026-01-05 14:25:56 +0100

    adding Marvin's Uuid wrapper

--- common/src/user.rs
+++ common/src/user.rs
@@ -1,3 +1,52 @@
+use diesel::deserialize::{self, FromSql};
+use diesel::serialize::{self, IsNull, Output, ToSql};
+use diesel::sql_types::Text;
+use diesel::sqlite::Sqlite;
+use diesel::{AsExpression, FromSqlRow};
+use std::fmt;
+use std::fmt::{Display, Formatter};
 use uuid::Uuid;
+
 /// Unique ID (UUID) for a user
 pub type UserId = Uuid;
+
+#[derive(Debug, Clone, Copy, FromSqlRow, AsExpression, Hash, Eq, PartialEq)]
+#[sql_type = "Text"]
+pub struct UUID(pub uuid::Uuid);
+
+// Small function to easily initialize our uuid
+impl UUID {
+    pub fn random() -> Self {
+        Self(uuid::Uuid::new_v4())
+    }
+}
+
+// Allow easy conversion from UUID to the wanted uuid::Uuid
+impl From<UUID> for uuid::Uuid {
+    fn from(s: UUID) -> Self {
+        s.0
+    }
+}
+
+impl Display for UUID {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl FromSql<Text, Sqlite> for UUID {
+    fn from_sql(
+        mut bytes: <Sqlite as diesel::backend::Backend>::RawValue<'_>,
+    ) -> deserialize::Result<Self> {
+        let text = bytes.read_text();
+        let uuid = Uuid::parse_str(text)?;
+        Ok(Self(uuid))
+    }
+}
+
+impl ToSql<Text, Sqlite> for UUID {
+    fn to_sql<'b>(&'b self, out: &mut Output<'b, '_, Sqlite>) -> serialize::Result {
+        out.set_value(format!("{}", self.0));
+        Ok(IsNull::No)
+    }
+}
