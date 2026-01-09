a7c3734fd60f31c31723ae1ef4374ddd686e4f07
mtfw <72407233+mtfwvi@users.noreply.github.com>
2026-01-09 03:02:21 +0100

    feat(client): Add logging via tracing
    Closes #18

--- client/src/main.rs
+++ client/src/main.rs
@@ -3,9 +3,24 @@ pub(crate) mod pages;
 
 use leptos::prelude::*;
 use pages::demo_page::DemoPage;
+use tracing_subscriber::fmt::format::DefaultFields;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
+use tracing_web::{MakeWebConsoleWriter, performance_layer};
 
 fn main() {
     console_error_panic_hook::set_once();
+
+    let fmt_layer = tracing_subscriber::fmt::layer()
+        .with_ansi(false)
+        .without_time()
+        .with_writer(MakeWebConsoleWriter::new());
+    let perf_layer = performance_layer().with_details_from_fields(DefaultFields::default());
+
+    tracing_subscriber::registry()
+        .with(fmt_layer)
+        .with(perf_layer)
+        .init();
 
     mount_to_body(DemoPage)
 }
