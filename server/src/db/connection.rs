8c2bef99534cee83683dadc32906d023f8868a28
Jonas Bretz <jbretz43@gmail.com>
2026-01-14 11:23:19 +0100

    adjustments regarding Marvin's comments

--- server/src/db/connection.rs
+++ server/src/db/connection.rs
@@ -1,26 +1,12 @@
 use diesel::prelude::*;
 use diesel::r2d2::{ConnectionManager, Pool};
-use dotenvy::dotenv;
-use std::env;
 
 pub type DbPool = Pool<ConnectionManager<SqliteConnection>>;
 
-pub fn establish_connection() -> SqliteConnection {
-    dotenv().ok();
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-
-    SqliteConnection::establish(&database_url)
-        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
-}
-
-pub fn create_pool() -> DbPool {
-    dotenv().ok();
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-
+pub fn create_pool(database_url: &str, pool_size: usize) -> DbPool {
     let manager = ConnectionManager::<SqliteConnection>::new(database_url);
-
     Pool::builder()
-        .max_size(15)
+        .max_size(pool_size.try_into().unwrap())
         .build(manager)
         .expect("Failed to create a pool")
 }
