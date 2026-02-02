a5044416d8986b87d405fcdc8ae4d06979907f25
Jonas Bretz <jbretz43@gmail.com>
2026-02-02 09:19:30 +0100

    feat(server) write the functions for the user repo

--- server/src/user/persistence/user_repository.rs
+++ server/src/user/persistence/user_repository.rs
@@ -1,6 +1,13 @@
+use crate::db::connection::DbPool;
+use crate::db::operations::{delete_user, insert_user, select_user, update_user};
+use crate::user::persistence::model::encryption_key::EncryptionKey;
 use crate::user::persistence::model::user_entity::UserEntity;
+use anyhow::Context;
 use anyhow::Result;
+use chrono::Utc;
+use crabdrive_common::user::UserType;
 use crabdrive_common::{data::DataAmount, uuid::UUID};
+use std::sync::Arc;
 
 pub(crate) trait UserRepository {
     fn create_user(
@@ -15,4 +22,59 @@ pub(crate) trait UserRepository {
     fn update_user(&self, updated_entity: UserEntity) -> Result<()>;
 
     fn delete_user(&self, id: UUID) -> Result<()>;
+}
+
+pub struct UserState {
+    db_pool: Arc<DbPool>,
+}
+
+impl UserState {
+    pub fn new(db_pool: Arc<DbPool>) -> Self {
+        Self { db_pool }
+    }
+}
+
+impl UserRepository for UserState {
+    fn create_user(
+        &self,
+        username: String,
+        password_hash: String,
+        storage_limit: DataAmount,
+    ) -> Result<UserEntity> {
+        let user = UserEntity {
+            user_type: UserType::User,
+            id: UUID::random(),
+            created_at: Utc::now().naive_utc(),
+            username,
+            password_hash,
+            storage_limit,
+            encryption_uninitialized: false,
+            master_key: EncryptionKey::nil(),
+            private_key: EncryptionKey::nil(),
+            public_key: Vec::new(),
+            root_key: EncryptionKey::nil(),
+            root_node: None,
+            trash_key: EncryptionKey::nil(),
+            trash_node: None,
+        };
+
+        insert_user(&self.db_pool, &user).context("Failed to insert user")?;
+        Ok(user)
+    }
+
+    fn get_user(&self, id: UUID) -> Result<UserEntity> {
+        select_user(&self.db_pool, id)
+            .context("Failed to select user")?
+            .context("User not found")
+    }
+
+    fn update_user(&self, updated_entity: UserEntity) -> Result<()> {
+        update_user(&self.db_pool, &updated_entity).context("Failed to update user")
+    }
+
+    fn delete_user(&self, id: UUID) -> Result<()> {
+        delete_user(&self.db_pool, id)
+            .context("Failed to delete user")
+            .map(|_| ())
+    }
 }
