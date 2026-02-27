9e59c75e156c5d4b1279eb4d3f1ce057dea50112
dercodeling <ichmagauchschnitzel@gmail.com>
2026-02-27 18:36:15 +0100

    refactor(client): Use providers for folder_selection_dialog

--- client/src/components/basic/folder_selection_dialog.rs
+++ client/src/components/basic/folder_selection_dialog.rs
@@ -1,5 +1,5 @@
-use crate::api::{get_children, path_to_root};
-use crate::components::basic::resource_wrapper::ResourceWrapper;
+use crate::components::data_provider::children_provider::ChildrenProvider;
+use crate::components::data_provider::path_provider::PathProvider;
 use crate::components::node_list::NodeList;
 use crate::components::path_breadcrumb::PathBreadcrumb;
 use crabdrive_common::storage::NodeId;
@@ -18,10 +18,6 @@ pub(crate) fn FolderSelectionDialog(
     start_folder: Signal<NodeId>,
 ) -> impl IntoView {
     let currently_open = RwSignal::new_local(start_folder.get());
-    let path_res = LocalResource::new(move || async move {
-        let node_id = currently_open.get();
-        path_to_root(node_id).await.map_err(|err| err.to_string())
-    });
 
     Effect::new(move || {
         if open.get() {
@@ -29,52 +25,29 @@ pub(crate) fn FolderSelectionDialog(
         }
     });
 
-    let set_open = Callback::new(move |node_id| {
-        currently_open.set(node_id);
-        path_res.refetch();
-    });
-
-    let handle_confirm = move || on_confirm.run(currently_open.get());
-
     view! {
+        // TODO: Extract dialog component to reduce indentation
         <Dialog open>
             <DialogSurface class="w-fit">
                 <DialogBody>
                     <DialogTitle>{title}</DialogTitle>
                     <DialogContent>
                         <div class="min-h-32 mt-2 p-6 rounded-sm outline outline-gray-300">
-                            <ResourceWrapper
-                                resource=path_res
-                                error_text=Signal::derive(move || {
-                                    format!(
-                                        "The path to node '{}' could not be loaded from the server",
-                                        currently_open.get(),
-                                    )
-                                })
-                                fallback_spinner=false
-                                children=move |path| {
-                                    let current_node = Signal::derive(move || {
-                                        path.get().last().expect("Failed due to empty path").clone()
-                                    });
-                                    let children_res = LocalResource::new(move || {
-                                        let current_node = current_node.get();
-                                        async move {
-                                            get_children(current_node)
-                                                .await
-                                                .map_err(|err| err.to_string())
-                                        }
+                            <PathProvider
+                                node_id=Signal::derive(move || currently_open.get())
+                                children=move |path, refetch_path| {
+                                    let set_open = Callback::new(move |node_id| {
+                                        currently_open.set(node_id);
+                                        refetch_path.run(());
                                     });
 
                                     view! {
-                                        <ResourceWrapper
-                                            resource=children_res
-                                            error_text=Signal::derive(move || {
-                                                format!(
-                                                    "The children of '{}' could not be loaded from the server",
-                                                    currently_open.get(),
-                                                )
+                                        <ChildrenProvider
+                                            node=Signal::derive(move || {
+                                                path.get().last().expect("Failed due to empty path").clone()
                                             })
                                             let:children
+                                            let:_refetch_children
                                         >
                                             <PathBreadcrumb path on_select=set_open compact=true />
                                             <Divider class="mb-2" />
@@ -84,7 +57,7 @@ pub(crate) fn FolderSelectionDialog(
                                                 on_folder_dblclick=set_open
                                                 folders_only=true
                                             />
-                                        </ResourceWrapper>
+                                        </ChildrenProvider>
                                     }
                                 }
                             />
@@ -99,7 +72,7 @@ pub(crate) fn FolderSelectionDialog(
                         </Button>
                         <Button
                             appearance=ButtonAppearance::Primary
-                            on_click=move |_| handle_confirm()
+                            on_click=move |_| on_confirm.run(currently_open.get())
                             disabled=Signal::derive(move || {
                                 start_folder.get() == currently_open.get()
                             })
