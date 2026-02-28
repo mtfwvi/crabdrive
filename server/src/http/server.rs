5c39f70d649e80fccb437ab34291e3ac13b2d8c0
mtfwvi <72407233+mtfwvi@users.noreply.github.com>
2026-02-28 12:34:23 +0100

    feat: Logout (#145)

--- server/src/http/server.rs
+++ server/src/http/server.rs
@@ -1,75 +1,53 @@
-use crate::db::connection::create_pool;
+use crate::db::operations;
 use crate::http::middleware::logging_middleware;
 use crate::http::{AppConfig, AppState, routes};
-use crate::storage::node::persistence::node_repository::NodeState;
-use crate::storage::revision::persistence::revision_repository::RevisionService;
-use crate::storage::share::persistence::share_repository::ShareRepositoryImpl;
-use crate::storage::vfs::backend::Sfs;
-use crate::user::auth::secrets::Keys;
-use crate::user::persistence::user_repository::UserState;
-
-use std::any::Any;
-use std::io::ErrorKind;
-use std::sync::Arc;
+use http_body_util::Full;
 
 use axum::http::StatusCode;
 use axum::http::header::{self, AUTHORIZATION, CONTENT_TYPE};
-use axum::middleware;
 use axum::response::Response;
+use axum::{Router, middleware};
 use bytes::Bytes;
-use http_body_util::Full;
+use std::any::Any;
+use std::io::ErrorKind;
+use tokio::time::Duration;
+use tokio::{task, time};
 use tower_http::catch_panic::CatchPanicLayer;
 use tower_http::cors::CorsLayer;
+use tracing::{error, info};
 
 async fn graceful_shutdown(state: AppState) {
     let _ = tokio::signal::ctrl_c().await;
     shutdown(state).await;
 }
 
-async fn shutdown(_state: AppState) {
-    tracing::info!("Stopping server");
-}
-
-pub async fn start(config: AppConfig) -> Result<(), ()> {
-    let pool = create_pool(&config.db.path, config.db.pool_size);
-
-    let vfs = Sfs::new(&config.storage.dir);
-
-    let node_repository = NodeState::new(Arc::new(pool.clone()));
-    let revision_repository = RevisionService::new(Arc::new(pool.clone()));
-    let user_repository = UserState::new(Arc::new(pool.clone()));
-    let share_repository = ShareRepositoryImpl::new(Arc::new(pool.clone()));
-
-    let keys = Keys::new(&config.auth.jwt_secret);
-
-    let state = AppState::new(
-        config.clone(),
-        pool,
-        vfs,
-        node_repository,
-        revision_repository,
-        user_repository,
-        share_repository,
-        keys,
-    );
+pub fn create_app(config: AppConfig) -> (Router, AppState) {
+    let state = AppState::new(config);
 
     let cors = CorsLayer::new() // TODO: Make more specific before submission
         .allow_origin(tower_http::cors::Any)
         .allow_methods(tower_http::cors::Any)
         .allow_headers([AUTHORIZATION, CONTENT_TYPE]);
 
-    let app = routes::routes()
+    let router = routes::routes()
         .with_state(state.clone())
         .layer(middleware::from_fn(logging_middleware))
         .layer(CatchPanicLayer::custom(handle_panic))
         .layer(cors);
+
+    (router, state)
+}
+
+pub async fn start(config: AppConfig) -> Result<(), ()> {
+    let (app, state) = create_app(config.clone());
+    let db_pool = state.db_pool.clone();
 
     let addr = config.addr();
 
     let listener = match tokio::net::TcpListener::bind(&addr).await {
         Ok(listener) => Ok(listener),
         Err(err) => {
-            tracing::error!(
+            error!(
                 "Failed to bind to {}. {}",
                 addr,
                 match err.kind() {
@@ -84,7 +62,32 @@ pub async fn start(config: AppConfig) -> Result<(), ()> {
         }
     }?;
 
-    tracing::info!("Server running on http://{}", &addr);
+    info!("Server running on http://{}", &addr);
+
+    task::spawn(async move {
+        let mut duration = time::interval(Duration::from_secs(60 * 15));
+        loop {
+            duration.tick().await;
+            tracing::info!("Removing expired tokens from blacklist");
+            let mut conn = match db_pool.get() {
+                Ok(conn) => conn,
+                Err(e) => {
+                    tracing::error!("Unable to remove expired tokens: {e}");
+                    break;
+                }
+            };
+            let now = chrono::Local::now().naive_utc();
+            // Ignore on error to prevent server crash
+            let count = operations::token::delete_expired_blacklisted_tokens(&mut conn, now)
+                .inspect_err(|e| {
+                    tracing::error!("Unable to remove expired tokens: {e}");
+                })
+                .ok()
+                .unwrap_or(0);
+
+            tracing::info!("Removed {count} tokens from blacklist!");
+        }
+    });
 
     axum::serve(listener, app)
         .with_graceful_shutdown(graceful_shutdown(state.clone()))
@@ -92,6 +95,10 @@ pub async fn start(config: AppConfig) -> Result<(), ()> {
         .unwrap();
 
     Ok(())
+}
+
+async fn shutdown(_state: AppState) {
+    info!("Stopping server");
 }
 
 // copied from here: https://docs.rs/tower-http/latest/tower_http/catch_panic/index.html
@@ -104,7 +111,7 @@ pub(crate) fn handle_panic(err: Box<dyn Any + Send + 'static>) -> Response<Full<
         "Unknown panic message".to_string()
     };
 
-    tracing::error!("Request handler panicked!: {:?}", details);
+    error!("panic: {:?}", details);
 
     let body = serde_json::json!({
         "error": {
--- server/src/user/mod.rs
+++ server/src/user/mod.rs
@@ -1,4 +1,8 @@
 pub(crate) mod auth;
 pub(crate) mod persistence;
 
+pub(crate) use persistence::model::token::SessionId;
+
+pub(crate) use persistence::model::token::BlacklistedTokenEntity;
+pub(crate) use persistence::model::token::RefreshTokenEntity;
 pub(crate) use persistence::model::user_entity::UserEntity;
